3bc90687873cf4f20da5fd4ba2be69633c282348
Joseph Acosta <allanacost.27@gmail.com>
2023-10-24 19:35:14 -0500

    React: useState en un componente padre pasando datos a un componente hijo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import "./app.css";
+import { useState } from "react";
 import Header from "./components/Header";
-import TaskList from "./components/TaskList";
 import Form from "./components/Form";
+import TaskList from "./components/TaskList";
+
+import "./app.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: "1", text: "Running" },
+    { id: "2", text: "Cooking" },
+    { id: "3", text: "Drawing" },
+    { id: "4", text: "Swimming" },
+  ]);
+
   return (
     <div className="app">
       <Header />
       <Form />
-      <TaskList />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
