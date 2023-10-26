89de3588cab6a4145504374f7acc878d089f7c0c
Joseph Acosta <allanacost.27@gmail.com>
2023-10-25 19:38:33 -0500

    React: Elimina una tarea

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,20 @@ function App() {
     { id: "4", text: "Swimming" },
   ]);
 
+  const onDeleteHandler = (id) => {
+    if (confirm("Are you sure ypu want to delete the task?")) {
+      // Elimina el elemento filtrando el arreglo por el id de cada tarea
+      const result = tasks.filter((task) => task.id !== id);
+      // Modifica el estado
+      setTasks(result);
+    }
+  };
+
   return (
     <div className="app">
       <Header />
       <AddTaskForm />
-      <TaskList tasks={tasks} />
+      <TaskList tasks={tasks} onDelete={onDeleteHandler} />
     </div>
   );
 }
