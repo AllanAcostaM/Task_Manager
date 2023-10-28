35d58518237e0e7d8cd4e948225404cf10c85235
Joseph Acosta <allanacost.27@gmail.com>
2023-10-28 08:34:43 -0500

    React: Se elimina una tarea en el backend usando Axios

--- task_manager/src/App.jsx
+++ task_manager/src/App.jsx
@@ -7,13 +7,12 @@ import "./styles/App.scss";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
   // useEffect se ejecuta una sola vez cuando se monta el componente
   useEffect(() => {
     const getTasks = async () => {
       try {
         // Hace la peticion al backend
-        const comunication = await axios.get("http://localhost:3000/tasks");
+        const comunication = await axios.get(import.meta.env.VITE_BACKEND_URL);
 
         // Verifica el exito de la peticion
         if (comunication.status === 200) {
@@ -28,31 +27,49 @@ function App() {
     getTasks();
   }, []);
 
-  const onDeleteHandler = (id) => {
+  const onDeleteHandler = async (id) => {
     if (confirm("Are you sure ypu want to delete the task?")) {
-      // Elimina el elemento filtrando el arreglo por el id de cada tarea
-      const result = tasks.filter((task) => task.id !== id);
-      // Modifica el estado
-      setTasks(result);
+      try {
+        // Elimina la tarea del backend
+        const comunication = await axios.delete(
+          `${import.meta.env.VITE_BACKEND_URL}${id}`
+        );
+        if (comunication.status === 200) {
+          // Elimina el elemento filtrando el arreglo por el id de cada tarea
+          const result = tasks.filter((task) => task.id !== id);
+          // Modifica el estado
+          setTasks(result);
+        }
+      } catch (error) {
+        alert("Error eliminado la tarea\nIntente mas tarde!!");
+        console.log(error);
+      }
     }
   };
 
   const onCreateHandler = async (text) => {
-    // Crean un objeto para la nueva tarea
-    const newTask = {
-      id: crypto.randomUUID(),
-      text,
-    };
-    // Crea la tarea en el backend
-    const comunication = await axios.post(
-      "http://localhost:3000/tasks",
-      newTask
-    );
-    // Crea un nuevo arreglo basado en los elementos del
-    // arreglo tasks agregando la nueva tarea al final
-    const newTasks = [...tasks, comunication.data];
-    // Modifica el estado
-    setTasks(newTasks);
+    try {
+      // Crean un objeto para la nueva tarea
+      const newTask = {
+        id: crypto.randomUUID(),
+        text,
+      };
+      // Crea la tarea en el backend
+      const comunication = await axios.post(
+        "http://localhost:3000/tasks/",
+        newTask
+      );
+      // Verificacion de exito de la operacion
+      if (comunication.status === 201) {
+        // Crea un nuevo arreglo basado en los elementos del arreglo tasks agregando la nueva tarea al final
+        const newTasks = [...tasks, comunication.data];
+        // Modifica el estado
+        setTasks(newTasks);
+      }
+    } catch (error) {
+      alert("Error de comunicacion creando la tarea\nIntente mas tarde!");
+      console.log("Hubo un error de comunicacion creando la tarea");
+    }
   };
 
   return (
